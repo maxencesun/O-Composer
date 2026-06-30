569140c3e01652b2e8a163a23148f185a4fd2f2e
Qixin Sun <278861054+maxencesun@users.noreply.github.com>
2026-06-30 19:59:17 +0800

    Bust Pages module cache

--- src/main.js
+++ src/main.js
@@ -339,7 +339,7 @@ requestAnimationFrame(() => {
   updateBootLoadingProgress({ percent: 18, detail: text.moduleDetail, indeterminate: true });
 });
 
-import("./ui/app-shell.js")
+import("./ui/app-shell.js?v=20260630-2")
   .then(({ PurplePenApp }) => {
     updateBootLoadingProgress({ percent: 52, detail: text.initializeDetail, indeterminate: false });
     customElements.define("purple-pen-app", PurplePenApp);

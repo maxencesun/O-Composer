3e9eaa238b7b3174f5ea28c05d4781209f10b56a
Qixin Sun <278861054+maxencesun@users.noreply.github.com>
2026-07-01 18:57:41 +0800

    Add parallel variation branch control

--- src/workers/omap-render-worker.js
+++ src/workers/omap-render-worker.js
@@ -1,4 +1,4 @@
-import { drawOmapMap } from "../ui/omap-renderer.js?v=20260701-6";
+import { drawOmapMap } from "../ui/omap-renderer.js?v=20260701-7";
 
 let currentMap = null;
 let currentMapVersion = 0;

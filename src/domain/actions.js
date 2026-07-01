46117742139cdf4f74389ebce4d5096951db3eda
Qixin Sun <278861054+maxencesun@users.noreply.github.com>
2026-07-01 13:47:31 +0800

    Log OMAP import diagnostics

--- src/domain/actions.js
+++ src/domain/actions.js
@@ -5,8 +5,8 @@ import {
   createSpecial,
   findById,
   nextId
-} from "./event-model.js?v=20260701-1";
-import { cloneDeep } from "./clone.js?v=20260701-1";
+} from "./event-model.js?v=20260701-2";
+import { cloneDeep } from "./clone.js?v=20260701-2";
 import {
   controlsUsedByCourse,
   courseGraphCourseControlIds,
@@ -15,7 +15,7 @@ import {
   getCourse,
   getCourseControl,
   sortedCourses
-} from "./course-service.js?v=20260701-1";
+} from "./course-service.js?v=20260701-2";
 
 export function addControlAt(eventModel, kind, location, selectedCourseId = null, options = {}) {
   const coursePlacement = controlCoursePlacement(kind, eventModel, selectedCourseId);
--- src/domain/event-model.js
+++ src/domain/event-model.js
@@ -1,4 +1,4 @@
-import { cloneDeep } from "./clone.js?v=20260701-1";
+import { cloneDeep } from "./clone.js?v=20260701-2";
 
 const DEFAULT_PRINT_AREA = Object.freeze({
   automatic: true,

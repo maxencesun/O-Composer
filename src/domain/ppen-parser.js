8f8ece0a339157a93efc188f0d2824e0bdf0f289
Qixin Sun <278861054+maxencesun@users.noreply.github.com>
2026-07-11 19:33:37 +0800

    Fix WebKit Mapper startup rejection

--- src/domain/ppen-parser.js
+++ src/domain/ppen-parser.js
@@ -3,7 +3,7 @@ import {
   defaultPrintArea,
   normalizeBool,
   normalizeNumber
-} from "./event-model.js?v=20260711-3";
+} from "./event-model.js?v=20260711-4";
 
 const BOX_ORDER = ["C", "D", "E", "F", "G", "H"];
 
